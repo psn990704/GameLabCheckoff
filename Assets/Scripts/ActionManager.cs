6d0160176c0363a77d78d029140b9962f0005e2d
psn990704 <psn990704@gmail.com>
2023-10-05 02:19:20 +0800

    Checkoff3

---
+++ Assets/Scripts/ActionManager.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ActionManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // triggered upon performed interaction (default successful press)
+    public void OnJump()
+    {
+        Debug.Log("OnJump called");
+        // TODO
+    }
+
+    // triggered upon 1D value change (default successful press and cancelled)
+    public void OnMove(InputValue input)
+    {
+        if (input.Get() == null)
+        {
+            Debug.Log("Move released");
+        }
+        else
+        {
+            Debug.Log($"Move triggered, with value {input.Get()}"); // will return null when released
+        }
+        // TODO
+    }
+
+    // triggered upon performed interaction (custom successful hold)
+    public void OnJumpHold(InputValue value)
+    {
+        Debug.Log($"OnJumpHold performed with value {value.Get()}");
+        // TODO
+
+    }
+    public void OnJumpHoldAction(InputAction.CallbackContext context)
+    {
+        if (context.started)
+            Debug.Log("JumpHold was started");
+        else if (context.performed)
+        {
+            Debug.Log("JumpHold was performed");
+        }
+        else if (context.canceled)
+            Debug.Log("JumpHold was cancelled");
+    }
+
+    // called twice, when pressed and unpressed
+    public void OnJumpAction(InputAction.CallbackContext context)
+    {
+        if (context.started)
+            Debug.Log("Jump was started");
+        else if (context.performed)
+        {
+            Debug.Log("Jump was performed");
+        }
+        else if (context.canceled)
+            Debug.Log("Jump was cancelled");
+
+    }
+
+    // called twice, when pressed and unpressed
+    public void OnMoveAction(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            Debug.Log("move started");
+            float move = context.ReadValue<float>();
+            Debug.Log($"move value: {move}"); // will return null when not pressed
+        }
+        if (context.canceled)
+        {
+            Debug.Log("move stopped");
+        }
+    }
+}
